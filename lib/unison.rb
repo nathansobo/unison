1e5522a00508243b7740d2f4e3387d647dc751e3
Damon McCormick & Nathan Sobo <pivotal@dorland.flood.pivotallabs.com>
2008-07-03 12:28:55 -0700

    No IntelliJ files. They cause pain.

--- lib/unison.rb
+++ lib/unison.rb
@@ -1,5 +1,6 @@
 require "unison/relations/set"
 require "unison/relations/selection"
+require "unison/relations/inner_join"
 require "unison/predicates"
 require "unison/attribute"
 require "unison/tuple"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,15 @@ Spec::Runner.configure do |config|
     users_set.attribute(:id)
     users_set.attribute(:name)
     @user_class = users_set.tuple_class
+
+    @photos_set = Unison::Set.new(:photos)
+    photos_set.attribute(:id)
+    photos_set.attribute(:user_id)
+    photos_set.attribute(:name)
+    @photo_class = photos_set.tuple_class
   end
 end
 
 class Spec::ExampleGroup
-  attr_reader :users_set, :user_class
+  attr_reader :users_set, :user_class, :photos_set, :photo_class
 end
