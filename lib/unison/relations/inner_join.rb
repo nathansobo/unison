2a01f2fd69a4c185136c2ffd329068f7edd5771c
Nathan Sobo & Brian Takita <grockit@Macpro03-desktop.(none)>
2008-07-29 11:53:53 -0700

    Refactoring InnerJoin operand subcriptions.

--- lib/unison/relations/inner_join.rb
+++ lib/unison/relations/inner_join.rb
@@ -9,40 +9,22 @@ module Unison
 
         operand_2.on_insert do |operand_2_tuple|
           operand_1.each do |operand_1_tuple|
-            compound_tuple = tuple_class.new(operand_1_tuple, operand_2_tuple)
-            if predicate.eval(compound_tuple)
-              tuples.push(compound_tuple)
-              trigger_on_insert(compound_tuple)
-            end
+            insert_if_predicate_matches tuple_class.new(operand_1_tuple, operand_2_tuple)
           end
         end
 
         operand_1.on_insert do |operand_1_tuple|
           operand_2.each do |operand_2_tuple|
-            compound_tuple = tuple_class.new(operand_1_tuple, operand_2_tuple)
-            if predicate.eval(compound_tuple)
-              tuples.push(compound_tuple)
-              trigger_on_insert(compound_tuple)
-            end
+            insert_if_predicate_matches tuple_class.new(operand_1_tuple, operand_2_tuple)
           end
         end
 
         operand_1.on_delete do |operand_1_tuple|
-          tuples.each do |compound_tuple|
-            if compound_tuple[operand_1] == operand_1_tuple
-              tuples.delete(compound_tuple)
-              trigger_on_delete(compound_tuple)
-            end
-          end
+          delete_if_member_of_compound_tuple operand_1, operand_1_tuple 
         end
 
         operand_2.on_delete do |operand_2_tuple|
-          tuples.each do |compound_tuple|
-            if compound_tuple[operand_2] == operand_2_tuple
-              tuples.delete(compound_tuple)
-              trigger_on_delete(compound_tuple)
-            end
-          end
+          delete_if_member_of_compound_tuple operand_2, operand_2_tuple
         end
       end
 
@@ -52,6 +34,22 @@ module Unison
 
       protected
       attr_reader :tuples
+
+      def insert_if_predicate_matches(compound_tuple)
+        if predicate.eval(compound_tuple)
+          tuples.push(compound_tuple)
+          trigger_on_insert(compound_tuple)
+        end
+      end
+
+      def delete_if_member_of_compound_tuple(operand, tuple)
+        tuples.each do |compound_tuple|
+          if compound_tuple[operand] == tuple
+            tuples.delete(compound_tuple)
+            trigger_on_delete(compound_tuple)
+          end
+        end
+      end
 
       def initial_read
         cartesian_product.select {|tuple| predicate.eval(tuple)}
