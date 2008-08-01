3bbf949d6ff5518fc0ca8fef4114b651c4b5fea9
Brian Takita <grockit@Macpro03-desktop.(none)>
2008-07-31 17:26:31 -0700

    Added Predicates::Or.

---
+++ lib/unison/predicates/or.rb
@@ -0,0 +1,25 @@
+module Unison
+  module Predicates
+    class Or < Base
+      attr_reader :child_predicates
+      def initialize(*child_predicates)
+        raise ArgumentError, "And predicate must have at least one child Predicate" if child_predicates.empty?
+        @child_predicates = child_predicates
+      end
+
+      def eval(tuple)
+        child_predicates.any? do |child_predicate|
+          child_predicate.eval(tuple)
+        end
+      end
+
+      def ==(other)
+        if other.is_a?(Or)
+          child_predicates == other.child_predicates
+        else
+          false
+        end
+      end
+    end
+  end
+end
--- spec/unit/predicates/or_spec.rb
+++ spec/unit/predicates/or_spec.rb
@@ -2,56 +2,57 @@ require File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper")
 
 module Unison
   module Predicates
-    describe And do
+    describe Or do
       attr_reader :predicate
 
       before do
-        @predicate = And.new(Eq.new(users_set[:id], 1), Eq.new(users_set[:name], "Nathan"))
+        @predicate = Or.new(Eq.new(users_set[:id], 3), Eq.new(users_set[:name], "Nathan"))
       end
 
       describe "#initialize" do
         context "when passed no arguments" do
           it "raises an ArgumentError" do
             lambda do
-              And.new
+              Or.new
             end.should raise_error(ArgumentError)
           end
         end
       end
 
       describe "#eval" do
-        context "when the passed in Tuple causes all of the child Predicates to #eval to true" do
+        context "when the passed in Tuple causes one of the child Predicates to #eval to true" do
           it "returns true" do
             user = User.find(1)
-            user.id.should == 1
+            user.id.should_not == 3
             user.name.should == "Nathan"
             predicate.eval(user).should be_true
           end
         end
 
-        context "when the passed in Tuple causes one of the child Predicates to not #eval to true" do
+        context "when the passed in Tuple causes none of the child Predicates to #eval to true" do
           it "returns false" do
             user = User.find(2)
-            user.name = "Nathan"
+            user.name.should_not == 3
+            user.name.should_not == "Nathan"
             predicate.eval(user).should be_false
           end
         end
       end
 
       describe "#==" do
-        context "when other And has the same #child_predicates" do
+        context "when other Or has the same #child_predicates" do
           it "returns true" do
-            predicate.should == And.new(Eq.new(users_set[:id], 1), Eq.new(users_set[:name], "Nathan"))
+            predicate.should == Or.new(Eq.new(users_set[:id], 3), Eq.new(users_set[:name], "Nathan"))
           end
         end
 
-        context "when other And does not have the same #child_predicates" do
+        context "when other Or does not have the same #child_predicates" do
           it "returns false" do
-            predicate.should_not == And.new(Eq.new(users_set[:id], 1))
+            predicate.should_not == Or.new(Eq.new(users_set[:id], 1))
           end
         end
-        
-        context "when other is not an And" do
+
+        context "when other is not an Or" do
           it "returns false" do
             predicate.should_not == Eq.new(users_set[:name], "Nathan")
           end
