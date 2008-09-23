b3d7d0a18c0d8c9c799662024dff021d67e4c7e3
Nathan Sobo & Brian Takita <grockit@Macpro03-desktop.(none)>
2008-09-22 18:35:52 -0700

    Added incomplete implementation of Topic.

---
+++ lib/unison/domains/topic.rb
@@ -0,0 +1,38 @@
+module Unison
+  module Domains
+    class Topic < Domain
+      class << self
+        def expose(*names)
+          exposed_method_names.concat(names)
+        end
+
+        protected
+        def exposed_method_names
+          @exposed_method_names ||= []
+        end
+      end
+
+      retain :exposed_objects
+
+      attr_reader :subject
+      def initialize(subject)
+        @subject = subject
+        super()
+      end
+
+      def exposed_objects
+        self.class.send(:exposed_method_names).map do |name|
+          self.send(name)
+        end
+      end
+
+      protected
+      def method_missing(method_name, *args, &block)
+        subject.send(method_name, *args, &block)
+      rescue NoMethodError => e
+        e.message.replace("undefined method `#{method_name}' for #{inspect}")
+        raise e
+      end
+    end
+  end
+end
